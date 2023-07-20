e0608e29c0456d259799d1c6e1a6ba66d2cfddcb
Aryan Trivedi <aryantrivedi208@gmail.com>
2023-07-20 16:32:47 +0530

    added database config and initialzation

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+const DB = process.env.MONGO_URL;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((conn) => console.log('Database Connected'));
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at ${PORT}`);
 });
