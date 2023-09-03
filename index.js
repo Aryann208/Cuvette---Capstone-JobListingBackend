2ce527bb0092cb680c8a099f46d4cb025439ec6e
Aryan Trivedi <aryantrivedi208@gmail.com>
2023-09-03 22:50:14 +0530

    CORS origin added

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const authRouter = require("./routes/authRoute");
-const jobRouter = require("./routes/jobRoute");
+const authRouter = require('./routes/authRoute');
+const jobRouter = require('./routes/jobRoute');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://cuvette-job-listing-frontend.onrender.com',
+  })
+);
 
 dotenv.config();
 
 // Health check endpoint
-app.get("/health", (req, res) => {
+app.get('/health', (req, res) => {
   const dbStatus =
-    mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+    mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected';
 
   res.status(200).json({
-    server: "Running",
+    server: 'Running',
     database: dbStatus,
   });
 });
 
-app.use("/", authRouter);
-app.use("/", jobRouter);
+app.use('/', authRouter);
+app.use('/', jobRouter);
 
 app.use((req, res, next) => {
-  const error = new Error("Not found");
+  const error = new Error('Not found');
   error.status = 404;
   next(error);
 });
@@ -40,7 +44,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res
     .status(500)
-    .json({ error: "Something went wrong! Please try again later." });
+    .json({ error: 'Something went wrong! Please try again later.' });
 });
 
 module.exports = app;
